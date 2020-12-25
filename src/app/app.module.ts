b3dd5b950dd0db8158ecec2d7cb5a13e6adab551
jainayushee3 <Ayushee.Jain@lntinfotech.com>
2020-12-25 21:46:24 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BlankComponent } from './blank/blank.component';
+import { SearchcardComponent } from './searchcard/searchcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BlankComponent,
+    SearchcardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchcard/searchcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchcard',
+  templateUrl: './searchcard.component.html',
+  styleUrls: ['./searchcard.component.css']
+})
+export class SearchcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
